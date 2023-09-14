4bc99947587d1fc668baae35c557e0b1d12e397c
fuki_fuki <fuuki1228@icloud.com>
2023-09-15 00:28:31 +0900

    add: header

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,27 @@
 import { createSignal } from "solid-js";
+import { css } from "solid-styled";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
+  css`
+    button {
+      font-family: inherit;
+      font-size: inherit;
+      padding: 1em 2em;
+      color: #335d92;
+      background-color: rgba(${String(count() * 30)}, 107, 158, 0.1);
+      border-radius: 2em;
+      border: 2px solid rgba(68, 107, 158, 0);
+      outline: none;
+      width: 200px;
+      font-variant-numeric: tabular-nums;
+    }
+
+    button:focus {
+      border: 2px solid #335d92;
+    }
+  `;
   return (
-    <button
-      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-      onClick={() => setCount(count() + 1)}
-    >
-      Clicks: {count()}
-    </button>
+    <button onClick={() => setCount(count() + 1)}>Clicks: {count()}</button>
   );
 }
--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,7 @@
 // @refresh reload
 import { Suspense } from "solid-js";
+import { useAssets } from "solid-js/web";
 import {
-  useLocation,
   A,
   Body,
   ErrorBoundary,
@@ -13,41 +13,130 @@ import {
   Scripts,
   Title,
 } from "solid-start";
-import "./root.css";
+import { css, renderSheets, StyleRegistry, type StyleData } from "solid-styled";
+
+import logo from "./assets/logo.jpg";
+
+function GlobalStyles() {
+  css`
+    @global {
+      body {
+        font-family: Gordita, Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+          "Helvetica Neue", sans-serif;
+        mergin: 0 !important;
+      }
+
+      a {
+        margin-right: 1rem;
+      }
+
+      main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+      }
+
+      header {
+        display: flex;
+        min-height: 10vh;
+        border: 0.0625rem solid #dee2e6;
+      }
+      #header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        max-width: 14rem;
+        gap: 8px;
+      }
+
+      h1 {
+        color: #335d92;
+        text-transform: uppercase;
+        font-size: 4rem;
+        font-weight: 100;
+        line-height: 1.1;
+        margin: 4rem auto;
+        max-width: 14rem;
+      }
+
+      h2 {
+        font-family: Open Sans;
+        -webkit-tap-highlight-color: transparent;
+        color: inherit;
+        font-size: inherit;
+        line-height: 1.55;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+        font-family: Open Sans;
+        font-weight: normal;
+        font-size: 1.375rem;
+        line-height: 1.4;
+        margin: 0;
+      }
+
+      p {
+        max-width: 14rem;
+        margin: 2rem auto;
+        line-height: 1.35;
+      }
+
+      img {
+        border-radius: 100%;
+        width: 100%;
+        max-width: 5rem;
+        height: auto;
+      }
+
+      @media (min-width: 480px) {
+        h1 {
+          max-width: none;
+        }
+
+        p {
+          max-width: none;
+        }
+      }
+    }
+  `;
+  return null;
+}
 
 export default function Root() {
-  const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+  const sheets: StyleData[] = [];
+  useAssets(() => renderSheets(sheets));
+
   return (
-    <Html lang="en">
-      <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                  <A href="/">Home</A>
-                </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
-                </li>
-              </ul>
-            </nav>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
+    <StyleRegistry styles={sheets}>
+      <Html lang="en">
+        <Head>
+          <Title>Maple</Title>
+          <Meta charset="utf-8" />
+          <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Body>
+          <GlobalStyles />
+          <Suspense>
+            <ErrorBoundary>
+              <header>
+                <div id="header">
+                  <img src={logo}></img>
+                  <h2>Maple</h2>
+                </div>
+              </header>
+              <div>
+                <A href="/">Index</A>
+                <A href="/about">About</A>
+              </div>
+              <Routes>
+                <FileRoutes />
+              </Routes>
+            </ErrorBoundary>
+          </Suspense>
+          <Scripts />
+        </Body>
+      </Html>
+    </StyleRegistry>
   );
 }
